32e9ba4dc6352e6cbbe6d449c5f90f3a6fff9e4f
Denis <normal64@mail.ru>
2023-10-04 20:06:45 +0200

    fix 1

--- wdio.conf.mjs
+++ wdio.conf.mjs
@@ -1,11 +1,11 @@
-// import baseConfig from './wdio.conf.base.js';
+import baseConfig from './wdio.conf.base.js';
 
-// export const config = {
-//   ...baseConfig,
+export const config = {
+  ...baseConfig,
 
-//   // Additional configuration specific to your environment can be added here
-// };
+  // Additional configuration specific to your environment can be added here
+};
 // wdio.conf.mjs
-import { config } from './wdio.config.js';
+// import { config } from './wdio.config.base.js';
 
-export default config;
+// export default config;
